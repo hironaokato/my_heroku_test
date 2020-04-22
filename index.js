3ba3e9552b20069a204d66603c849c9149afa00a
Kobayashi Rui <roboroborx782@gmail.com>
2020-04-22 16:24:41 +0900

    update db query

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express')
+const pg = require('pg')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var pool = pg.Pool({
+    database:'', 
+    user:'',
+    password:'',
+    host:'',
+    port:,
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/get_data', (req, res) => {
+    pool.connect((err,client)=>{
+        if(err){
+            console.log(err)
+        }else{
+            client.query("SELECT name FROM users", function(err, result){
+                res.send(JSON.stringify(result))
+            })
+        }
+    })
+})
 
 app.listen(PORT)
